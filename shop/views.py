9cb62384c66750b1689319c522118f4f504b81aa
potapchukdmytro <traiget@gmail.com>
2025-06-26 15:59:34 +0300

    test main

--- shop/views.py
+++ shop/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Product  # ОБОВ’ЯЗКОВО
 
 def home(request):
+    a = 1
     products = Product.objects.all()
     return render(request, 'shop/home.html', {'products': products})
